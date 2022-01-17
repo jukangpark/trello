26be59ca54f530be86061f90e24a7fd3093c7fdf
jukangpark <skyxxx9339@naver.com>
2022-01-17 16:54:15 +0900

    cross board

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,7 @@ const App = () => {
   const onDragEnd = (info: DropResult) => {
     console.log(info);
     const { destination, draggableId, source } = info;
+    if (!destination) return;
     if (destination?.droppableId === source.droppableId) {
       // same board movement
       setToDos((allBoards) => {
@@ -48,6 +49,20 @@ const App = () => {
         // 2) Put back item on the destination.index
         boardCopy.splice(destination?.index, 0, draggableId);
         return { ...allBoards, [source.droppableId]: boardCopy };
+      });
+    }
+    if (destination.droppableId !== source.droppableId) {
+      // cross board movement
+      setToDos((allBoards) => {
+        const sourceBoard = [...allBoards[source.droppableId]];
+        const destinationBoard = [...allBoards[destination.droppableId]];
+        sourceBoard.splice(source.index, 1);
+        destinationBoard.splice(destination?.index, 0, draggableId);
+        return {
+          ...allBoards,
+          [source.droppableId]: sourceBoard,
+          [destination.droppableId]: destinationBoard,
+        };
       });
     }
   };
